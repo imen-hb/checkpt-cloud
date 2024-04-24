8290db887e8428ce24ae7b0a02b1134a8f835a88
imen-hb <imenhbibi502@gmail.com>
2024-04-25 00:04:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GoogleMaps from './Components/Maps';
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleMaps />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Maps.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const GoogleMaps = () => {
+  return (
+
+        <div>
+            <h1>Google Maps Integration</h1>
+        <iframe title='maps' width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=gomycode%20menzah&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+        </iframe>
+        <a href="https://123movies-to.org">
+        </a>
+        </div>
+
+
+  )
+}
+
+export default GoogleMaps
